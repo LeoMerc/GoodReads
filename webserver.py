29db891821ae0d4b853486e3a19b50bb978afa46
leonardo mercado <leonardoomercado2@gmail.com>
2023-09-25 16:53:40 -0600

    changed ip

--- webserver.py
+++ webserver.py
@@ -16,7 +16,7 @@ load_dotenv()
 
 mapping = [
     (r"^/api/books/search", "get_api_search"),
-    (r"^/api/books/suggestion", "get_book_suggestion"),
+    (r"^/api/books/suggestion", "get_suggestion"),
     (r"^/books/(?P<book_file>.+)$", "get_book"),
     (r"^/$", "get_index"),
     (r"^/index$", "get_index"),
